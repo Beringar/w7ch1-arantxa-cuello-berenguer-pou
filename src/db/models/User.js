47fab43f27c6487029068283e8f782a9640f5f11
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-23 18:08:12 +0100

    add model User in /db/models

---
+++ src/db/models/User.js
@@ -0,0 +1,22 @@
+const { model, Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  admin: { type: Boolean },
+  series: [{ type: Schema.Types.ObjectId, ref: "Serie" }],
+});
+
+const User = model("User", UserSchema, "users");
+
+module.exports = User;
