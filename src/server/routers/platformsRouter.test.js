841d83314b8261bbe5286e13b62505aea1974b17
Arantxa Cuello <arantxa_312@hotmail.com>
2022-02-24 18:49:39 +0100

    put id platform created

--- src/server/routers/platformsRouter.test.js
+++ src/server/routers/platformsRouter.test.js
@@ -75,3 +75,16 @@ describe("Given a /platforms endpoint", () => {
     });
   });
 });
+
+describe("Given a platforms/:idPlatform endpoint", () => {
+  describe("When it receives a PUT request with a valid token", () => {
+    test("Then it should response with a code 200", async () => {
+      const token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ikx1aXM4IiwibmFtZSI6Ikx1aXMgUCIsImlkIjoiNjIxNmE1ZmQ4MzYyMDM1YTBhMzhlMTE0IiwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjQ1NzIyOTAzfQ.YuxggXPlm0H60vHXduQVPVnmauSbGevZNYUtqgIiauA";
+      await request(app)
+        .put("/platforms/:idPlatform")
+        .set("Authorization", `${"Bearer"} ${token}`);
+      expect(200);
+    });
+  });
+});
